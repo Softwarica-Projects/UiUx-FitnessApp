76c4792c542a0f056a70ddf2d480b0aa7388056c
Rishan <shrestha.rishan99@gmail.com>
2025-12-27 11:21:28 +0545

    added logout option service to clear cache and clear store data permanently

---
+++ frontend/lib/service/auth_service.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import '../extensions/shared_pref.dart';
+import '../main.dart';
+import '../utils/app_constants.dart';
+
+Future<void> logout(BuildContext context, {Function? onLogout}) async {
+  await removeKey(IS_LOGIN);
+  await removeKey(USER_ID);
+  await removeKey(FIRSTNAME);
+  await removeKey(LASTNAME);
+  await removeKey(USER_PROFILE_IMG);
+  await removeKey(DISPLAY_NAME);
+  await removeKey(PHONE_NUMBER);
+  await removeKey(GENDER);
+  await removeKey(AGE);
+  await removeKey(HEIGHT);
+  await removeKey(HEIGHT_UNIT);
+  await removeKey(IS_OTP);
+  await removeKey(WEIGHT);
+  await removeKey(WEIGHT_UNIT);
+  userStore.clearUserData();
+  if (!getBoolAsync(IS_REMEMBER) || getBoolAsync(IS_OTP) == true) {
+    await removeKey(PASSWORD);
+    await removeKey(EMAIL);
+  }
+  userStore.setLogin(false);
+  onLogout?.call();
+}
