5004e1fac6f9160fb04c99a6d93e15adab8a976f
Rishan <shrestha.rishan99@gmail.com>
2025-12-27 12:18:43 +0545

    created levels list screen with level component

--- frontend/lib/network/rest_api.dart
+++ frontend/lib/network/rest_api.dart
@@ -1,9 +1,12 @@
 import 'package:habit_tracker/languageConfiguration/ServerLanguageResponse.dart';
 import 'package:habit_tracker/models/base_response.dart';
+import 'package:habit_tracker/models/level_response.dart';
+import 'package:habit_tracker/utils/app_config.dart';
 
 import '../demoData.dart';
 import '../models/login_response.dart';
 
+///[Auth]
 Future<LoginResponse> logInApi(request) async {
   await demoDelay();
   return LoginResponse.fromJson(demoUser.toJson());
@@ -14,16 +17,12 @@ Future<LoginResponse> registerApi(Map req) async {
   return LoginResponse.fromJson(demoUser.toJson());
 }
 
-Future<LoginResponse> updateProfileApi(Map req) async {
-  await demoDelay();
-  return LoginResponse.fromJson(demoUser.toJson());
-}
-
 Future<FitnessBaseResponse> forgotPwdApi(Map req) async {
   await demoDelay();
   return FitnessBaseResponse.fromJson({"message": "Password reset link sent"});
 }
 
+///[Localization]
 Future<ServerLanguageResponse> getLanguageList(versionNo) async {
   await demoDelay();
   return ServerLanguageResponse.fromJson({
@@ -31,6 +30,7 @@ Future<ServerLanguageResponse> getLanguageList(versionNo) async {
   });
 }
 
+///[Settings]
 Future<FitnessBaseResponse> changePwdApi(Map req) async {
   await demoDelay();
   return FitnessBaseResponse.fromJson({"message": "Password changed successfully"});
@@ -39,4 +39,14 @@ Future<FitnessBaseResponse> changePwdApi(Map req) async {
 Future<FitnessBaseResponse> deleteUserAccountApi() async {
   await demoDelay();
   return FitnessBaseResponse.fromJson({"message": "User account deleted"});
+}
+
+Future<LoginResponse> updateProfileApi(Map req) async {
+  await demoDelay();
+  return LoginResponse.fromJson(demoUser.toJson());
+}
+
+///[Levels]
+Future<LevelResponse> getLevelListApi({int? page = 1, int mPerPage = LEVEL_PER_PAGE}) async {
+  return LevelResponse.fromJson(await fakePagination(data: demoLevels.map((e) => e.toJson()).toList(), page: page ?? 1));
 }
